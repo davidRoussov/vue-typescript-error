2e86f07bce86d950f66ebabb7a3d4f3f1cac0250
David Roussov <david.roussov@gmail.com>
2021-10-24 18:56:42 +1100

    add FooBar

---
+++ src/components/FooBar.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <p>{{ bar }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export const FooBar = Vue.extend({
+  computed: {
+    bar: function() {
+      return this.foo;
+    }
+  },
+  data: function() {
+    return {
+      foo: 'bar',
+    };
+  },
+});
+
+export default FooBar;
+</script>
